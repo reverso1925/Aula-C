14b5e183e2ed8c7a62bba235f53b2762562a7d51
reverso1925 <reverso1925@protonmail.com>
2025-08-01 09:56:37 -0300

    Update ex_07.c

--- ex_07.c
+++ ex_07.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+int main() {
+    char nome[50];
+    float preço;
+    int quantidade;
+    printf("Insira o nome do produto: ");
+    scanf("%s", nome);
+    printf("Insira o preço unitário do produto: ");
+    scanf("%f", &preço);
+    printf("Insira a quantidade: ");
+    scanf("%d", &quantidade);
+    float total =(preço * quantidade);
+    printf("%d unidades do produto %s custam R$%.2f\n", quantidade, nome, total);
+}
