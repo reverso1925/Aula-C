5afbbcfa1a867e76aba0cb788afa2ecca15ebd01
reverso1925 <reverso1925@protonmail.com>
2025-08-01 09:47:46 -0300

    Update ex_06.c

--- ex_06.c
+++ ex_06.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+int main(){
+    char nome[50];
+    float salario;
+    int meses;
+    printf("Insira o nome do funcionário: ");
+    scanf("%49s", nome);
+    printf("Insira o salário mensal: ");
+    scanf("%f", &salario);
+    printf("Insira a quantidade de meses: ");
+    scanf("%d", &meses);
+    float total =(salario * meses);
+    printf("Funcionário %s ganhou R$%.3f em %d meses\n", nome, total, meses);
+}
