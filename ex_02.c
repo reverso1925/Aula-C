0ffd987be09d4593693ae293b8b06903b4414b73
reverso1925 <reverso1925@protonmail.com>
2025-08-01 08:21:14 -0300

    Update ex_02.c

--- ex_02.c
+++ ex_02.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main(){
+    int numero1, numero2;
+    int soma = 0;
+    int contagem = 0;
+    printf("Digite o primeiro número inteiro: ");
+    scanf ("%d", &numero1);
+    printf("Você digitou: %d\n", numero1);
+    printf("Digite o segundo número inteiro: ");
+    scanf ("%d", &numero2);
+    printf("Você digitou: %d\n", numero2);
+    soma =numero1 + numero2;
+    printf("A soma dos dois números é: %d\n", soma);
+}
