1e1b31ef64c1a6e00ba9e5e3c06c7f661d8f58ef
reverso1925 <reverso1925@protonmail.com>
2025-08-01 09:59:56 -0300

    Update ex_03.c

--- ex_03.c
+++ ex_03.c
@@ -3,13 +3,12 @@ int main(){
     float nota1 = 0;
     float nota2 = 0;
     float nota3 = 0;
-    float media;
     printf("Digite a primeira nota: \n");
     scanf("%f", &nota1);
     printf("Digite a segunda nota: \n");
     scanf("%f", &nota2);
     printf("Digite a terceira nota: \n");
     scanf("%f", &nota3);
-    media =(nota1 + nota2 + nota3) / 3;
+    float media =(nota1 + nota2 + nota3) / 3;
     printf("A média do aluno é: %.2f\n", media);
 }
