8a1780a263719cc40d47d22f47618e6b64dc2561
reverso1925 <reverso1925@protonmail.com>
2025-08-01 08:32:29 -0300

    Update ex_03.c

--- ex_03.c
+++ ex_03.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+int main(){
+    int nota1;
+    int nota2;
+    int media;
+    printf("Digite a primeira nota: \n");
+    scanf("%d", &nota1);
+    printf("Digite a segunda nota: \n");
+    scanf("%d", &nota2);
+    media =(nota1 + nota2) / 2;
+    printf("A média do aluno é: %d\n", media);
+}
