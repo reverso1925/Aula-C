f18543cec5175eb3df6fc9289159ba55f159c791
reverso1925 <reverso1925@protonmail.com>
2025-08-01 09:24:20 -0300

    Update ex_04.c

--- ex_04.c
+++ ex_04.c
@@ -3,14 +3,14 @@ int main(){
     float metro;
     float cm;
     char escolha;
-    printf("Para converter centímetros para metros pressione A, caso queira converter metros para centímetros pressione B");
+    printf("Para converter centímetros para metros pressione A, caso queira converter metros para centímetros pressione B: \n");
     scanf("%c", &escolha);
-    if (escolha == 'a') {
+    if (escolha == 'A') {
         printf("Digite a quantidade em centímetros: ");
         scanf("%f", &cm);
         float resultado = cm / 100;
         printf("%.2f centímetros é igual a %.2f metros\n", cm, resultado);
-        } else if (escolha) == 'b' {
+        } else if (escolha== 'B') {
             printf("Digite a quantiade em metros: ");
             scanf("%f", &metro);
             float resultado = metro * 100;
